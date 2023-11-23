9ddbef46b22514f8d4d68543b89a812bea32937c
luozhiheng <651716183@qq.com>
2023-11-23 23:04:46 +0800

    add paypal s3 upload

--- medusa-config.js
+++ medusa-config.js
@@ -35,11 +35,26 @@ const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
 const plugins = [
   `medusa-fulfillment-manual`,
-  `medusa-payment-manual`,
-  {
-    resolve: `@medusajs/file-local`,
+  //`medusa-payment-manual`,
+  // {
+  //   resolve: `@medusajs/file-local`,
+  //   options: {
+  //     upload_dir: "uploads",
+  //   },
+  // },
+   {
+    resolve: `medusa-file-s3`,
     options: {
-      upload_dir: "uploads",
+        s3_url: process.env.S3_URL,
+        bucket: process.env.S3_BUCKET,
+        region: process.env.S3_REGION,
+        access_key_id: process.env.S3_ACCESS_KEY_ID,
+        secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+        cache_control: process.env.S3_CACHE_CONTROL,
+        // optional
+        // download_file_duration:
+        //   process.env.S3_DOWNLOAD_FILE_DURATION,
+        // prefix: process.env.S3_PREFIX,
     },
   },
   {
@@ -50,6 +65,16 @@ const plugins = [
       develop: {
         open: process.env.OPEN_BROWSER !== "false",
       },
+    },
+  },
+
+  {
+    resolve: `medusa-payment-paypal`,
+    options: {
+      sandbox: process.env.PAYPAL_SANDBOX,
+      clientId: process.env.PAYPAL_CLIENT_ID,
+      clientSecret: process.env.PAYPAL_CLIENT_SECRET,
+      authWebhookId: process.env.PAYPAL_AUTH_WEBHOOK_ID,
     },
   },
 ];
